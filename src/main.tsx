2ba0fb3a993ae2819bfacd9824128857090a9d50
alaawymage <alaawymage@gmail.com>
2025-11-02 00:33:17 +0300

    fix(build): ensure react-router-dom in deps and lockfile

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import App from "./App";
-import Login from "./pages/Login";
-import AdminPage from "./pages/Admin";
+import Login from "@/pages/Login";
+import AdminPage from "@/pages/Admin";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
